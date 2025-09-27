1ca0c8db018ea17768a72666ec8a6383c218c92f
Manasa <manasaallaparthi@gmail.com>
2025-09-27 18:41:23 +0530

    variables

---
+++ 02.Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to alaska $PERSON2"
+echo "$PERSON2:: Thankyou, Finally not on a zoom webinar"
+echo "$PERSON1:: So, do we have a deal"
+echo "$PERSON2:: yeah, if you make ukraine to join Russia"
